496725e5f6c9e72bea89fdf462b19315ab73d853
caleb kandoro <kandoroc@hotmail.com>
2020-02-24 15:36:36 +0200

    db util string

--- database/database_util.py
+++ database/database_util.py
@@ -158,6 +158,7 @@ def create_new_database():
     # install fixtures
     subprocess.run(['python', 'manage.py', 'loaddata', 'accounts.json', 'journals.json', 'settings.json','common.json', 'employees.json', 'inventory.json', 'invoicing.json', 'planner.json', 'data.json'])# obtained from the data dump
 
+    #python manage.py loaddata accounts.json journals.json settings.json common.json employees.json inventory.json invoicing.json planner.json
     print("Database created successfully.")
     if os.path.exists('data.json'):
         os.remove('data.json')
