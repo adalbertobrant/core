815724bbf14f63a843dcd89a8381bbcc0f0eb9b6
Caleb Kandoro <kandoroc@hotmail.com>
2020-03-02 16:16:42 +0200

    changes to setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ import datetime
 
 #create .env first!
 with open('latrom/settings/.env', 'w') as f:
-    secret_key = input('enter sercret key') 
-    email_key = input('enter email key')
+    secret_key = input('enter sercret key: ') 
+    email_key = input('enter email key: ')
+    email_user = input('enter email user: ')
     f.write(f"SECRET_KEY='{secret_key}'")
     f.write(f"EMAIL_KEY='{email_key}'")
+    f.write(f"EMAIL_USER='{email_user}'")
 
 with open('database/config.json', 'w') as f:
     json.dump({'current': 'db.sqlite3'}, f)
@@ -30,11 +32,8 @@ with open('license.json', 'w') as f:
         "timestamp": datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
     }}, f)
 
-subprocess.run('mkvirtualenv env --python=/usr/bin/python3.6'.split(' '))
+# subprocess.run('mkvirtualenv env --python=/usr/bin/python3.6'.split(' '))
 subprocess.run('pip install -r requirements.txt'.split(' '))
-
-
-
 
 subprocess.run('python manage.py makemigrations'.split(' '))
 subprocess.run('python manage.py migrate'.split(' '))
