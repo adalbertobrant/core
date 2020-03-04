94b5a3f5b535593fca6e5ac152a4671004001205
Caleb Kandoro <kandoroc@hotmail.com>
2020-03-04 22:12:00 +0200

    pygal font

--- common_data/utilities/plotting.py
+++ common_data/utilities/plotting.py
@@ -4,6 +4,7 @@ CustomStyle = BlueStyle(
     label_font_size=16,
     major_label_font_size=20,
     legend_font_size=16,
+    font_family='sans-serif',
     title_font_size=20,
     guide_stroke_dasharray='0',
     major_guide_stroke_dasharray='0',
